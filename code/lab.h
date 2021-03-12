c0d8ba6e3e48c8d6c3c503ff6cbed545413dd911
RobinHYuan <68177491+RobinHYuan@users.noreply.github.com>
2021-03-12 01:40:25 -0800

    Add files via upload

--- code/lab.h
+++ code/lab.h
@@ -389,26 +389,25 @@ float Ch1_Frequency()
 	return Frequency;
 }
 
-float phase_Det()
-{
-	float ak = 0, ps = 0, fenn = 0;
-	TR0 = 0;
-	TMOD &= 0B_1111_0000;
-	TMOD |= 0B_0000_0001;
-	TH0 = 0; TL = 0; ak = 0;
-	TF = 0;
+float Ch2_Frequency()
+{	
+	float myof=0,Period,Frequency;
 
-	while (P2_2==1);
-	while (P2_2==0);
-
-	while (P2_3 != 1) {
-		if (TF0) { TF0 = 0; ak++; }
+	TR0=0;
+	TMOD&=0B_1111_0000; // Set timer 0 as 16-bit timer (step 1)
+	TMOD|=0B_0000_0001; // Set timer 0 as 16-bit timer (step 2)
+	TH0=0; TL0=0; myof=0; // Reset the timer and overflow counter
+	TF0=0; // Clear overflow flag
+	while (P2_3==1); // Wait for the signal to be zero
+	while (P2_3==0); // Wait for the signal to be one
+	TR0=1; // Start timing
+	while (P2_3	==1) // Wait for the signal to be zero
+	{
+	if (TF0) {TF0=0; myof++; }
 	}
-	TR0 = 0;
+	TR0=0; // Stop timer 0
 
-	ps = (ak * 0x10000L + TH0 * 0x100L + TL0) * 2L * 12 / 72000000.0;
-	fenn = Ch1_frequency();
-	per = 1 / fenn;
-	return ps / per * 360.0;
-
+	Period=(myof*0x10000L+TH0*0x100L+TL0)*2L*12/72000000.0;
+	Frequency=1.0/Period;
+	return Frequency;
 }
